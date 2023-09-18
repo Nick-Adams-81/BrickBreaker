7a930b85b0274e7f500cd31298570bf5899a29c6
Nick Adams <nick.pappas.adams@gmail.com>
2023-09-17 20:42:57 -0700

    paint method

--- src/Gameplay.java
+++ src/Gameplay.java
@@ -1,10 +1,50 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class Gameplay extends JPanel implements KeyListener, ActionListener {
+
+    private boolean play = false;
+    private int score = 0;
+    private int totalBricks = 21;
+    private Timer time;
+    private int delay = 8;
+    private int playerX = 310;
+    private int ballPositionX = 120;
+    private int ballPositionY = 350;
+    private int ballXDir = -1;
+    private int ballYDir = -2;
+
+    public Gameplay() {
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        time = new Timer(delay, this);
+        time.start();
+    }
+
+    public void paint(Graphics g) {
+        // background
+        g.setColor(Color.BLACK);
+        g.fillRect(1, 1, 692, 592);
+
+        // borders
+        g.setColor(Color.YELLOW);
+        g.fillRect(0, 0, 692, 3);
+        g.fillRect(691, 0, 3, 592);
+
+        // the paddle
+        g.setColor(Color.GREEN);
+        g.fillRect(playerX, 550, 100, 8);
+
+        // the ball
+        g.setColor(Color.YELLOW);
+        g.fillRect(ballPositionX, ballPositionY, 20, 20);
+
+    }
 
 
     @Override
